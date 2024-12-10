d36ab84c8cf5966a26b0ef5ac27d1b6329787ebe
Alex-de-bug <367193@edu.itmo.ru>
2024-12-10 15:29:07 +0300

    fix inject

--- lab3spring/backend/src/main/java/com/alwx/backend/configs/MinioConfig.java
+++ lab3spring/backend/src/main/java/com/alwx/backend/configs/MinioConfig.java
@@ -1,23 +1,23 @@
 package com.alwx.backend.configs;
 
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import io.minio.MinioClient;
-
+import lombok.Getter;
+import lombok.Setter;
 
 @Configuration
+@ConfigurationProperties(prefix = "spring.minio")
+@Getter
+@Setter
 public class MinioConfig {
     
-    @Value("${MINIO_ENDPOINT}")
     private String endpoint;
-    
-    @Value("${MINIO_ACCESS_KEY}")
     private String accessKey;
-    
-    @Value("${MINIO_SECRET_KEY}")
     private String secretKey;
+    private String bucket;
     
     @Bean
     public MinioClient minioClient() {
